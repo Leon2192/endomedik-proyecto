12aef03ddc0ece7b7cf6194dc46ce323728bd19e
leon_2192 <leon.strokes@gmail.com>
2025-01-15 18:12:54 -0300

    fix handle notfound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,26 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
+import Image from "next/image";
 
 const NotFound = () => {
   return (
-    <div>not-found</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+      <h3 className="text-2xl font-semibold text-gray-700">
+          Oooops!! Página no encontrada
+        </h3>
+        <div className="relative w-96 h-96 mx-auto">
+          <Image
+            src="/images/notfound.png"
+            alt="Página no encontrada"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
